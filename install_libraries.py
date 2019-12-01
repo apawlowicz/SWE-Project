c2335fa648e267a1fb215b1b7a2a6cedefde46fb
wfraher <williamfraher@my.ccsu.edu>
2019-12-01 15:25:35 -0500

    adding a new library to the install script

--- install_libraries.py
+++ install_libraries.py
@@ -3,6 +3,7 @@ from os import system
 system("py -m pip install setuptools")
 system("py -m pip install virtualenv")
 system("py -m pip install Flask")
+system("py -m pip install flask-cors")
 system("py -m pip install flask flask-jsonpify flask-sqlalchemy flask-restful")
 system("py -m pip install pandas")
 system("py -m pip install numpy")
